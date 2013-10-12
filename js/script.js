7b1655f1ce63e45875f7953b2e71ccf369a80a7d
MichaelFrizzell <MichaelFrizzell@fullsail.edu>
2013-10-12 18:20:18 -0400

    Initial_Setup
    Getting everything to sync together, figured out issue.

---
+++ js/script.js
@@ -0,0 +1,25 @@
+//Expressions - Assignment Operators
+
+var a = 3;
+a += 4;  //a = a + 4
+console.log(a);
+
+var a = 3;
+a *= 4;  //a = a * 4
+console.log(a);
+
+var a = 3;
+a /= 4;  //a = a / 4
+console.log(a);
+
+var a = 3;
+a -= 4;  //a = a - 4
+console.log(a);
+
+var a = 3;
+a ++ ;  //a = a + 1 // a += 1 // same as --//  Only works with 1
+console.log(a);
+
+
+//this changes the css of your body tag to be green
+document.querySelector("body").style.backgroundColor= "#00FF00";
